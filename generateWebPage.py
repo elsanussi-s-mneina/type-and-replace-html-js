d45e858b02bd1d31a4314d00d1a461593a8d5131
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2021-03-04 15:32:47 -0600

    remove dead code

--- generateWebPage.py
+++ generateWebPage.py
@@ -1,22 +1,9 @@
 
 # Uses template.html as a template, and replaces some text in it to make a new web page.
 # for a language.
-import sys
 
-
-def generateWebPage(dirName):
-    orthographyName = open(dirName + '/name.txt').read()
-    titled = sys.stdin.read().replace('ISO-233 Arabic Romanization Template', orthographyName)
-    replacements = open(dirName + '/replacements.txt').read()
-    print(titled.replace('[[\'11\', \'2\']]', replacements))
-
- 
 def generateWebPageAndSave(dirName, templateFileContents):
     orthographyName = open(dirName + '/name.txt').read()
     titled = templateFileContents.replace('ISO-233 Arabic Romanization Template', orthographyName)
     replacements = open(dirName + '/replacements.txt').read()
     return titled.replace('[[\'11\', \'2\']]', replacements)
-
-if __name__ == "__main__":
-    dirName = sys.argv[1] # 1st argument: should be name of directory in the current directory
-    generateWebPage(dirName)
