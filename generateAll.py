06e7856312a15c7437e991754deaa2cd154acc90
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2021-03-04 15:59:18 -0600

    Refactor: rename

--- generateAll.py
+++ generateAll.py
@@ -1,59 +1,97 @@
 
-# Uses template.html as a template, and replaces some text in it to make a new web page.
+# Replaces some text in it to make a new web page.
 # for a language.
 import sys
 from typing import List
 from generateWebPage import fillTemplate
 
-_typeAndReplaceTemplateFileName: str = "type_and_replace_template.html"
-_typeAndClickTemplateFileName: str = "type_and_click_template.html"
-_generatedPath = "generated/"
+# You will need to modify the following line if you add or remove a language (directory).
+_languageDirectories: List[str] = ["berber-latin-alphabet","french","german","icelandic","irish","iso233","latin","maltese","mehri-soqotri","pinyin","polish","qamus-buckwalter","qamus-buckwalter-inverse","scottish-gaelic","spanish", "portuguese"]
 
-def getTypeAndReplaceHTMLPageName(language: str):
+
+_typeAndReplaceTemplatePath: str = "type_and_replace_template.html"
+_typeAndClickTemplatePath: str = "type_and_click_template.html"
+_generatedPath: str = "generated/"
+
+def typeAndReplaceFileName(language: str) -> str:
+    """
+    The name of the file where the Type
+    and replace web application should be saved.
+    """
     return language + "QuickTypist.html"
 
-def getTypeAndClickHTMLPageName(language: str):
+def typeAndClickFileName(language: str) -> str:
+    """
+    The name of the file where the Type and
+    Click web application should be saved
+    """
     return language + "ClickTypist.html"
 
-def getTypeAndReplaceHTMLPagePath(language: str):
-    return _generatedPath + getTypeAndReplaceHTMLPageName(language)
+def typeAndReplacePath(language: str) -> str:
+    """
+    The place where the Type and Replace page
+    should be saved in this repository.
+    """
+    return _generatedPath + typeAndReplaceFileName(language)
 
-def getTypeAndClickHTMLPagePath(language: str):
-    return _generatedPath + getTypeAndClickHTMLPageName(language)
+def typeAndClickPath(language: str) -> str:
+    """ The place where the Type and Click page
+    should be saved in this repository
+    """
+    return _generatedPath + typeAndClickFileName(language)
 
 
-def generateIndexPage(directories: List[str]):
-    fileContents = ""
+def generateIndexPage(directories: List[str]) -> str:
+    """
+    Generates a page with a link to all the other generated pages.
+    It only generates the contents of the body.
+    """
+    fileContents: str = ""
     for directory in directories:
-        fileContents += "<li><a href='" + getTypeAndReplaceHTMLPageName(directory) +"'>" + directory + "(quick)</a></li>\n"
-        fileContents += "<li><a href='" + getTypeAndClickHTMLPageName(directory) +"'>" + directory + " (simple)</a></li>\n"
+        fileContents += "<li><a href='" + typeAndReplaceFileName(directory) +"'>" + directory + "(quick)</a></li>\n"
+        fileContents += "<li><a href='" + typeAndClickFileName(directory) +"'>" + directory + " (simple)</a></li>\n"
 
     open(_generatedPath + "index.html", "w").write(fileContents)
 
 
-def generateWebPages(directories: List[str]):
+def generateAll(directories: List[str]) -> None:
+    """
+    Generate the single page web app pages,
+    and an index page.
+    """
     generateTypeAndReplaceWebPages(directories)
     generateTypeAndClickWebPages(directories)
     generateIndexPage(directories)
 
 
-def generateTypeAndReplaceWebPages(directories):
-    templateContents = open(_typeAndReplaceTemplateFileName, 'r').read()
+def generateTypeAndReplaceWebPages(directories) -> None:
+    """
+    Generate web pages where the user types text,
+    and some of it is replaced as they type,
+    or after they press a button.
+    """
+    templateContents = open(_typeAndReplaceTemplatePath, 'r').read()
 
     for directory in directories:
         genFileContents = fillTemplate(directory, templateContents)
-        open(getTypeAndReplaceHTMLPagePath(directory), 'w').write(genFileContents)
+        open(typeAndReplacePath(directory), 'w').write(genFileContents)
 
 
-def generateTypeAndClickWebPages(directories):
-    templateContents = open(_typeAndClickTemplateFileName, 'r').read()
+def generateTypeAndClickWebPages(directories) -> None:
+    """
+    Generates web pages where the user types text,
+    and for characters that do not exist on their
+    keyboard (such as C with a cedilla),
+    the user can click on a button near the
+    text area.
+    """
+    templateContents = open(_typeAndClickTemplatePath, 'r').read()
 
     for directory in directories:
         genFileContents = fillTemplate(directory, templateContents)
-        open(getTypeAndClickHTMLPagePath(directory), 'w').write(genFileContents)
+        open(typeAndClickPath(directory), 'w').write(genFileContents)
 
 
+# The following runs if this module is run as the main module:
 if __name__ == "__main__":
-    directories = ["berber-latin-alphabet","french","german","icelandic","irish","iso233","latin","maltese","mehri-soqotri","pinyin","polish","qamus-buckwalter","qamus-buckwalter-inverse","scottish-gaelic","spanish", "portuguese"]
-    
-    generateWebPages(directories)
+    generateAll(_languageDirectories)
